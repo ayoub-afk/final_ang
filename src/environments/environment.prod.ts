eccb3d64eb6cd3804fcf04b8aa4ead0af6a4be64
unknown <ayoubmessaadi9@gmail.com>
2022-08-08 17:19:24 +0300

    add fire base

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
+  firebase: {
+    projectId: 'store-77ccc',
+    appId: '1:72537087598:web:abcb399e5ab6f51b4c507f',
+    storageBucket: 'store-77ccc.appspot.com',
+    apiKey: 'AIzaSyAKH_YP_H8njhcBkFhuDAVhxErl9jty524',
+    authDomain: 'store-77ccc.firebaseapp.com',
+    messagingSenderId: '72537087598',
+  },
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'store-77ccc',
+    appId: '1:72537087598:web:abcb399e5ab6f51b4c507f',
+    storageBucket: 'store-77ccc.appspot.com',
+    apiKey: 'AIzaSyAKH_YP_H8njhcBkFhuDAVhxErl9jty524',
+    authDomain: 'store-77ccc.firebaseapp.com',
+    messagingSenderId: '72537087598',
+  },
   production: false
 };
 
